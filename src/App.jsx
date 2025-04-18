503804bbe3b9e5ac13264426c4fe3173f86e0a86
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-04-18 16:18:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl"> hello </h1>
+      <button className="btn btn-accent">hey</button>
+    </>
+  );
+}
+
+export default App;
